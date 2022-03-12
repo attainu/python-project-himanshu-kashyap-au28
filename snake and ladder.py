f5bbea8f27f8250bcb7f870b180850b773366be1
himanshu-kashyap-au28 <akkukashyap9@gmail.com>
2022-03-12 05:34:06 +0530

    modified project

--- snake and ladder.py
+++ snake and ladder.py
@@ -147,7 +147,6 @@ def check_win(player_name, position):
     if MAX_VAL == position:
         print("\n\n\nThats it.\n\n" + player_name + " won the game.")
         print("Congratulations " + player_name)
-        print("\nThank you for playing the game. Please visit https://www.pythoncircle.com\n\n")
         sys.exit(1)
 
 
